7431f2a588b516b0a30c2dd90a7e62d44c4d23ad
fomm1 <90146133+fomm1@users.noreply.github.com>
2024-01-23 13:54:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage() {
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.textContent = "";
+    imageView.style.border ="0";
+    
+}
